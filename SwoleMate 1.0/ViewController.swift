5447bad40ee3c4870524de85d60aaded09a88957
Kevan Wang <kevanwang@gmail.com>
2018-08-12 02:12:20 -0700

    Initial Commit

---
+++ SwoleMate 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwoleMate 1.0
+//
+//  Created by Kevan Wang on 8/12/18.
+//  Copyright © 2018 Kevan Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
